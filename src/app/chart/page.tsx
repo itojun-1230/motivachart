da8256024f710638e41169be9fc7be97ac6ba935
itojun-1230 <juntou1230@gmail.com>
2024-12-25 11:59:27 +0900

    add: 新しいチャートページコンポーネントを追加

---
+++ src/app/chart/page.tsx
@@ -0,0 +1,14 @@
+import { PageHeading, Stack, Text } from "smarthr-ui"
+
+const ChartPage: React.FC = () => {
+  return (
+    <main>
+      <Stack>
+        <PageHeading>モチベーショングラフ</PageHeading>
+        <Text>モチベーショングラフを作成するページです。</Text>
+      </Stack>
+    </main>
+  )
+}
+
+export default ChartPage
